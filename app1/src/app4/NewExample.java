231527bc13d34ae8053be3707b1b87847115eac2
charan <charanpathapalyam@ouklook.com>
2022-08-24 22:37:37 +0530

    first commit

---
+++ app1/src/app4/NewExample.java
@@ -0,0 +1,13 @@
+package app4;
+
+public class NewExample {
+	
+public static void main(String[] args) {
+	System.out.println("charan");
+	N.data();
+}
+	public  void data()
+	{
+		System.out.println("classdata");
+}
+}
