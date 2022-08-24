88dfac6ce86aa6f1ada12e7ca15f427835cb733e
charan <charanpathapalyam@outlook.com>
2022-08-24 20:39:11 +0530

    inital commit

---
+++ app/src/app/Constructor.java
@@ -0,0 +1,22 @@
+package app;
+
+public class Constructor {
+	public static void main(String[] args) {
+		test();
+		Constructor c = new Constructor();
+		c.test1();
+	   }
+	   public static void test()
+	   {
+	   System.out.println("charan");
+	   }
+	    public Constructor()
+	    {
+	       System.out.println("murali");
+	    }
+	    public void test1()
+	    {
+	    	System.out.println("ravi");
+}
+	    
+}
